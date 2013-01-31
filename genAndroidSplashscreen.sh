cb7d450bb1b17999f7fc1a3da3f2119868bca31d
Luke GJ Potter <lukegjpotter@gmail.com>
2013-01-31 21:32:14 +0000

    Adding the generation script
    Signed-off-by: Luke GJ Potter <lukegjpotter@gmail.com>

---
+++ genAndroidSplashscreen.sh
@@ -0,0 +1,36 @@
+#/bin/bash
+#--------------------------------------
+#
+#		genAndroidSplashscreen.sh
+#
+#		Author: Luke Potter
+#		GitHub: lukegjpotter
+#
+#		Date: 31/Jan/2013
+#
+#--------------------------------------
+#		Version: 1.0
+#--------------------------------------
+#
+#		Description:
+#			A script to generate the
+#			code required for a splash
+#			screen for an Android app.
+#
+#--------------------------------------
+
+# Constants
+SUCCESS=0
+BAD_ARGS=1
+
+# Functions
+
+
+#--------------------------------------
+# Main , i.e. the Stagelist
+#--------------------------------------
+evaluate_args
+
+
+# exit successfully
+exit $SUCCESS
